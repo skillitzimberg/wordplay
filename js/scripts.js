535fe83ecff3929e2220e85b921fd4e261bded1a
Mary McMurray and Scott Bergler <mary@>
2018-10-22 15:43:22 -0700

    add working functionality up to third bulletpoint of exercise.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+  $("form#input").submit(function(event){
+    event.preventDefault();
 
+    var inputSentence = $("input#sentence").val();
+    var sentenceWords = inputSentence.split(" ");
+    var words = [];
+
+    var threePlusLetterWords = sentenceWords.forEach(function(word) {
+      
+      if (word && word.length >= 3) {
+        words.push(word);
+
+      }
+         console.log(words);
+    });
+
+    console.log(threePlusLetterWords);
+  });
 
 });
