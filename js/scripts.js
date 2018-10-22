1bf30bd87f3cecee032a5a525f37b70bc16b7ee1
Mary McMurray and Scott Bergler <scott@>
2018-10-22 16:45:30 -0700

    move brackets

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,25 @@
+var suits = ["clubs", "diamonds", "hearts", "spades"];
+var faces = ["ace", "2", "3", "4", "5", "6","7", "8", "9", "10", "jack", "queen", "king"];
+var cards = [];
+
 $(document).ready(function() {
-  var suits = ["clubs", "diamonds", "hearts", "spades"];
-  var faces = ["ace", "2", "3", "4", "5", "6","7", "8", "9", "10", "jack", "queen", "king"];
-  var cards = [];
 
   $("#deck").submit(function(event) {
     event.preventDefault();
-
+    console.log("hi");
     suits.forEach(function(suit) {
       faces.forEach(function(face) {
         cards.push(face + " of " + suit);
         return cards;
 
       });
-
+    });
+    cards.forEach(function(card) {
+      console.log(card);
+      $("#fullDeck").append("<li>" + card + "</li>");
     });
 
   });
-  cards.forEach(function(card) {
-    $("#fullDeck").append("<li>" + card "</li>");
-  });
+
 
 });
